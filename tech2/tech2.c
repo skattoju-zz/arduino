1e1fd4426934fd4158880240b72126c6e8c947e5
chuck <chuck@tcf-laptop.(none)>
2011-02-08 14:30:04 -0500

    changed tech2.c for assignment2

--- tech2/tech2.c
+++ tech2/tech2.c
@@ -13,13 +13,16 @@ int inc = 0;
 volatile int enable_B2 = 0b00000000;
 
 int main() {
-// setting up external interrupt to trigger at falling edge
+// setting up external interrupt to trigger at logical change
       MCUCR |= 0<<1;
       MCUCR |= 1<<0;
-	  //enable external interrupt
+
+//enable external interrupt
       GICR |= 0b1 << 6;
 
-sei();
+	// global interrupts enable
+	sei();
+
 // setting up timer interrupt
           TCCR1A = 0b0000000;
 	  TCCR1B = 0b0001010;
@@ -46,24 +49,15 @@ sei();
 // main loop that polls B2
      while(1){
 
-	    //poll = (PIND >> 3) & 0b1;
             if (((PIND & 0b00001000) == 0b00000000)) {
-			_delay_ms(100);
+			_delay_ms(50);
 			while (((PIND & 0b00001000) == 0b00000000)) {
 					_delay_ms(1);
 				}
-                //toggle timer interrupts
-                //TCNT1 = 0x01;
-			TIMSK = TIMSK ^ (1 << 4);
-			_delay_ms(100);
-			
-		serial_print("this is team scam\r\n\0");
-		void print_adc();
-
+		//an object was detected
+		serial_print("object detected");
             }
        }
-
-
 	return 0;
 }
 
@@ -72,49 +66,15 @@ ISR(INT0_vect) {
 	// Delay (for debouncing)
 	_delay_ms(50);
 
-	// if pin is high (on release)
+	// if pin is high, there is an object
 	if ((PIND & 0b00000100) == 0b00000100) {
-		// disable timer0 interrupt
-		TIMSK = 0 << 4;
-
-		switch (inc) {
-		// First click
-		// start alternation of LEDs
-			case 0:
-				// accept clicks from B2
-				//enable_B2 = 1;
-				if ((TIMSK & (1 << 4)) != 0b00010000) {
-				turnOnLed2();
-				}
-				// enable timer0 interrupt
-				TIMSK = 1<<4;
-				// reset timer
-				TCNT1 = 0x01;
-				break;
-		// second click
-		// turn LEDs on
-			case 1:
-
-			   	 turnOnBothLeds();
-				 break;
-		// turn LEDs off
-			case 2:
-				 turnOffBothLeds();
-				 break;
-		// turn LEDs on
-			case 3:
-				  turnOnBothLeds();
-				  break;
-		// LED1 off, LED2 on
-			case 4:
-				  turnOffLed1();
-				  turnOnLed2();
-		// disable external interrupt for B1
-				GICR = 0 << 6;
-				break;
-		}
-      inc++;
-	  }
+		// so turn on an led
+		turnOnLed1();
+	// if pin is low, there is no object
+	else {
+		// so turn off led
+		turnOffLed1();
+	}
 
 	  // Clear the external interrupt flag.
 	  // Used for debouncing, cancels any
