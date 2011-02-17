432d2be81dfd940f35f82014794d5432908f09f6
Siddhartha Kattoju <siddharthakattoju@gmail.com>
2011-02-17 04:18:25 -0500

    saving changes before merging with chucks code..

--- tech2/tech2_util.c
+++ tech2/tech2_util.c
@@ -1,8 +1,21 @@
-extern char *itoa( int number ) {
+#include <stdlib.h>
 
-                /* convert ADC value to ascii for UART */
-		char* buffer[4];
+char buffer[4];
+
+/* C does not support function overloading
+
+this behavior may be simulated by the following:
+
+printf style functions (type as an argument)
+opengl style functions (type in function name)
+c subset of c++ (if You can use a c++ compiler)
+
+*/
+
+extern char* itoa1(int number){
+
+                /* convert ADC value to ascii for UART  */
+
                 itoa (number, buffer, 10);
-		return (char*(buffer));
-
+		return((char*)buffer);
 }
